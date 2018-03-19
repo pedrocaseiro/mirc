1c4d302f0aee9f2460831e3f7147e257cf12e4e0
pedrocaseiro <pcaseiro96@gmail.com>
2018-03-19 22:52:47 +0000

    Chat working, needs message persistency

---
+++ assets/js/mirc.js
@@ -0,0 +1,29 @@
+let Mirc = {
+  init(socket) {
+    let channel = socket.channel('mirc:lobby', {});
+    channel.join();
+    this.listenForChats(channel);
+  },
+
+  listenForChats(channel) {
+    document.getElementById('chat-form').addEventListener('submit', function(e){
+      e.preventDefault();
+      let userName = document.getElementById('user-name').value;
+      let userMsg = document.getElementById('user-msg').value;
+
+      channel.push('shout', {name: userName, body: userMsg});
+
+      document.getElementById('user-name').value = '';
+      document.getElementById('user-msg').value = '';
+    })
+
+    channel.on('shout', payload => {
+      let chatBox = document.querySelector('#chat-box');
+      let msgBlock = document.createElement('p');
+      msgBlock.insertAdjacentHTML('beforeend', `${payload.name}: ${payload.body}`);     
+      chatBox.appendChild(msgBlock);
+    })
+  }
+}
+
+export default Mirc
